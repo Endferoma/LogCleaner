81e4b489605acb15970e9509eb4e41b86b56c89a
Endferoma <cloudworkend@gmail.com>
2024-06-08 17:17:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using SharpCompress.Common;
+using SharpCompress.Compressors.Xz;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+
+string folderPath = "logs";
+string[] regexPattern = File.ReadAllLines("patterns.txt");
+
+List<FileInfo> logs = new DirectoryInfo(folderPath).GetFiles().ToList();
+Regex[] compiledPatterns = regexPattern.Select(p => new Regex(p, RegexOptions.Compiled)).ToArray();
+
+foreach (var log in logs) {
+    using (var streamReader = new StreamReader(log.FullName)) {
+        if (log.Extension == ".gz") {
+            using (var archive = new GZipStream(streamReader.BaseStream, CompressionMode.Decompress))
+            using (var archiveReader = new StreamReader(archive)) {
+                ReadFileStream(archiveReader);
+            }
+        } else {
+            ReadFileStream(streamReader);
+        }
+    }
+
+void ReadFileStream(StreamReader reader) {
+        string line;
+        
+        while ((line = reader.ReadLine()) != null) {
+            foreach (var pattern in compiledPatterns) {
+                var match = pattern.Match(line);
+                if (match.Success) {
+                    //Console.WriteLine($"Matched: {match.Groups["player1"].Value}");
+                    string comment = match.Groups["comment"].Value;
+                    line = comment == "" ? line : line.Replace(comment, "");
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
+
